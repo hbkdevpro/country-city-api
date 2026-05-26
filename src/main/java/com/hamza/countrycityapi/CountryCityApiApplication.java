56427be62fe3397f01f879bf7957104dcbee5cb5
h-bahraoui <hamza.bahraoui.k@gmail.com>
2026-05-26 18:54:16 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/hamza/countrycityapi/CountryCityApiApplication.java
@@ -0,0 +1,13 @@
+package com.hamza.country_city_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCityApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamza/country_city_api/CountryCityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamza.country_city_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryCityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
